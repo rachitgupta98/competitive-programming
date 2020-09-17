3bd08bd0ea65f7a8b4a11a39dd261db3fead5b36
rachitgupta98 <rachit371@gmail.com>
2020-09-17 21:38:41 +0530

    Problem 6

--- String/Rearrange String having dissimilar adjacent.cpp
+++ String/Rearrange String having dissimilar adjacent.cpp
@@ -1,44 +1,43 @@
-
 //https://leetcode.com/problems/reorganize-string/
 
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-	string S;
-	cin >> S;
-	int len = S.length();
-	map<char, int> m;
-	int max = INT_MIN;
-	char k;
-	for (char x : S)
-	{
-		m[x]++;
-		if (m[x] > max)
-		{
-			max = m[x];
-			k = x;
-		}
-	}
-	if (max - 1 > len - max)
-		cout << "";
-	int j = 0;
-	while (max--)
-	{
-		S[j] = k;
-		j += 2;
-	}
-	for (int e = 0; e < 26; e++)
-	{
+    string S;
+    cin >> S;
+    int len = S.length();
+    map<char, int> m;
+    int max = INT_MIN;
+    char k;
+    for (char x : S)
+    {
+        m[x]++;
+        if (m[x] > max)
+        {
+            max = m[x];
+            k = x;
+        }
+    }
+    if (max - 1 > len - max)
+        cout << "";
+    int j = 0;
+    while (max--)
+    {
+        S[j] = k;
+        j += 2;
+    }
+    for (int e = 0; e < 26; e++)
+    {
 
-		while ('a' + e != k && m['a' + e]-- > 0)
-		{
-			if (j >= S.size())
-				j = 1;
-			S[j] = 'a' + e;
-			j += 2;
-		}
-	}
+        while ('a' + e != k && m['a' + e]-- > 0)
+        {
+            if (j >= S.size())
+                j = 1;
+            S[j] = 'a' + e;
+            j += 2;
+        }
+    }
 
-	cout << S;
+    cout << S;
 }
